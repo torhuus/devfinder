da3406d394e4d4ed6b5f16939063278df5aae31d
Tor Einar Huus <tor@huus.me>
2021-11-06 22:48:26 +0100

    added styled-components and configured initial darkmode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './components/globalStyles';
+import { lightTheme, darkTheme } from './components/Theme';
+
 
 function App() {
-  return (
-    <div className="App">
-	<h1>Welcome to GitHub API-based developer finder</h1>
-    </div>
-  );
+	const [theme, setTheme] = useState('light');
+
+	const themeToggler = () => {
+		theme === 'light' ? setTheme('dark') : setTheme('light');
+	}
+
+	return (
+		<>
+			<ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+				<GlobalStyles />
+				<div className="App">
+					<button onClick={() => themeToggler()}>Change me</button>
+					<h1>Welcome to GitHub API-based developer finder</h1>
+				</div>
+			</ThemeProvider>
+		</>
+	);
 }
 
 export default App;
---
+++ todo.md
@@ -0,0 +1,4 @@
+# Todolist
+- [ ] This is awesome, learn how to configure this more and set up the same environment using ansible
+- [ ] Do some easy, but sweet looking frontendmentor stuff
+- [ ] Focus on frontend, that's what is fun. But also look into creating my own api for consuming data from Withings and others
